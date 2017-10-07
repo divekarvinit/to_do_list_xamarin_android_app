cf4601b6e5c14e18b22ac5e5c27d0aa049fa7ac0
Savi <30487435+savi029@users.noreply.github.com>
2017-10-07 18:30:25 +1300

    SearchName

---
+++ MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using System.Collections;
+using System.Reflection.Emit;
+
+namespace SearchName
+{
+    [Activity(Label = "SearchName", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public object HorizontalOptions { get; private set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            ArrayList names = SearchName.Search("name");
+
+            var buttons = FindViewById<LinearLayout>(Resource.Id.Buttons);
+
+            foreach (var i in names)
+            {
+                Label person = new Label
+                {
+                    Text = i.ToString()
+
+                };
+                var button = new Button(this);
+                button.Text = "ADD ";
+                button.LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent,
+                                                                     ViewGroup.LayoutParams.WrapContent);
+
+            }
+        }
+    }
+}
+
---
+++ searchName.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System;
+using System.Collections;
+
+namespace Core
+{
+    public static class SearchName
+    {
+        public static ArrayList Search(string raw)
+        {
+            
+            ArrayList names = new ArrayList();
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+            else
+                raw = raw.ToUpperInvariant();
+            ArrayList search = new ArrayList(); //database search should go here;
+            foreach (var i in search)
+            {
+                if (i.ToString().Contains(raw)) {
+                    names.Add(i);
+                }
+                
+            }
+            return names;
+        }
+    }
+}
